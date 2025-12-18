7f72c22f098661977c4f9c8aa02bb35882784632
MartinGauster <martin.gau48@gmail.com>
2025-12-18 11:59:49 +0000

    Test

--- foo.c
+++ foo.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
-
+#include <math.h>
 int main(void)
 {
     printf("%s\n", "Hallo Welt!");
+
+    printf("Aufgabe 1: Weihnachtskekse\n");
+    
+    int number_biscuits = 0;
+    float diameter_biscuit = 0;
+    float area = 0;
+
+    area = (pow(diameter_biscuit, 2) * M_PI) /4;
+
+    printf("Fuer die Glasur aller %d Kekse wird Schokolade für %f [cm²] benoetigt");
+
+    printf("Aufgabe 2: Christbaumschmuck");
+
+    int candles = 0;
+    int balls = 0;
+
+    candles = 1,50;
+    balls = 4,90;
+
+    float Gesamtkosten = 0;
+
+    printf("Die Gesamtkosten für den Christbaumschmuck betragen", candles * 1,50 + balls * 4,90);
+
+
+
+    
+
+
+
+
+
+
+
+
 }
